3c569fb751ff6d6fd77cf1278725f074448e1eec
Viacheslav Litvinov <viacheslav@hot-now.com>
2020-05-24 14:30:56 +0700

    Terminate on failure

--- main.cpp
+++ main.cpp
@@ -24,15 +24,27 @@ void Assert(bool b, const std::string& hint) {
 }
 
 
-template <class TestFunc>
-void RunTest(TestFunc func, const std::string& test_name) {
-  try {
-    func();
-    std::cout << test_name << " PASSED" << std::endl;
-  } catch (std::runtime_error& e) {
-    std::cout << test_name << " FAILED: " << e.what() << std::endl;
+class TestRunner {
+public:
+  template <class TestFunc>
+  void RunTest(TestFunc func, const std::string& test_name) {
+    try {
+      func();
+      std::cerr << test_name << " PASSED" << std::endl;
+    } catch (std::runtime_error& e) {
+      ++fail_count;
+      std::cerr << test_name << " FAILED: " << e.what() << std::endl;
+    }
   }
-}
+  ~TestRunner() {
+    if ( fail_count > 0 ) {
+      std::cerr << fail_count << " test failed. Terminate";
+      exit(1);
+    }
+  }
+private:
+  int fail_count = 0;
+};
 
 
 void TestMapsAreEqual() {
@@ -47,14 +59,18 @@ void BooleanAssertion() {
   std::map<std::string, uint16_t> m2 { {"first", 1}, {"second", 3} };
 
   Assert(true, "Assert boolean");
+}
 
+
+void TestAll() {
+  TestRunner tr;
+  tr.RunTest(TestMapsAreEqual, "TestMapsAreEqual");
+  tr.RunTest(BooleanAssertion, "BooleanAssertion");
 }
 
 
 int main() {
-
-  RunTest(TestMapsAreEqual, "TestMapsAreEqual");
-  RunTest(BooleanAssertion, "BooleanAssertion");
+  TestAll();
 
   return 0;
 }
