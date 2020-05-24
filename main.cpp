75001caeff6408487f484559d757c16c8ad36260
Viacheslav Litvinov <viacheslav@hot-now.com>
2020-05-24 14:10:30 +0700

    Make test cases isolated

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 
 #include "operators_overloading.cpp"
 
+
 template <class T, class U>
 void AssertEqual(const T& t,
 		 const U& u,
@@ -23,18 +24,37 @@ void Assert(bool b, const std::string& hint) {
 }
 
 
-int main() {
+template <class TestFunc>
+void RunTest(TestFunc func, const std::string& test_name) {
+  try {
+    func();
+    std::cout << test_name << " PASSED" << std::endl;
+  } catch (std::runtime_error& e) {
+    std::cout << test_name << " FAILED: " << e.what() << std::endl;
+  }
+}
 
+
+void TestMapsAreEqual() {
   std::map<std::string, uint16_t> m1 { {"first", 1}, {"second", 2} };
   std::map<std::string, uint16_t> m2 { {"first", 1}, {"second", 3} };
 
-  AssertEqual(0, 0, "Assert equality");
   AssertEqual(m1, m2, "Assert equality for maps");
+}
+
+void BooleanAssertion() {
+  std::map<std::string, uint16_t> m1 { {"first", 1}, {"second", 2} };
+  std::map<std::string, uint16_t> m2 { {"first", 1}, {"second", 3} };
 
   Assert(true, "Assert boolean");
 
-  
+}
+
+
+int main() {
+
+  RunTest(TestMapsAreEqual, "TestMapsAreEqual");
+  RunTest(BooleanAssertion, "BooleanAssertion");
 
   return 0;
-
 }
